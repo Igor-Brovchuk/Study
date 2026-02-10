7a2a8e7cf4537f55f683c53205f63bcc200e09d9
Igor-Brovchuk <i.brovchuk@gmail.com>
2026-02-10 16:55:39 +0200

    update

---
+++ js/task-1.js
@@ -0,0 +1,17 @@
+const name = "Igor";
+const age = 42;
+
+console.log(name);
+console.log('Мені ' + age + ' роки');
+
+const x = 5;
+const y = 10;
+const addition = x + y;
+const subtraction = x - y;
+const multiplication = x * y;
+const division = x / y;
+
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
