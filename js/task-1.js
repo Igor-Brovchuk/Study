4f1546377e11e0b7d3c76579c53ba511be3b9ac5
Igor-Brovchuk <i.brovchuk@gmail.com>
2026-02-11 16:45:21 +0200

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,17 +1,118 @@
-const name = "Igor";
-const age = 42;
+// const name = "Igor";
+// const age = 42;
+// console.log(name);
+// console.log('Мені ' + age + ' роки');
 
-console.log(name);
-console.log('Мені ' + age + ' роки');
+// const x = 5;
+// const y = 10;
+// const addition = x + y;
+// const subtraction = x - y;
+// const multiplication = x * y;
+// const division = x / y;
+// console.log(addition);
+// console.log(subtraction);
+// console.log(multiplication);
+// console.log(division);
 
-const x = 5;
-const y = 10;
-const addition = x + y;
-const subtraction = x - y;
-const multiplication = x * y;
-const division = x / y;
+// const city = 'Rivne';
+// console.log('Я живу в місті ' + city);
 
-console.log(addition);
-console.log(subtraction);
-console.log(multiplication);
-console.log(division);
+// const price = 500;
+// console.log('Ціна ' + price + 'грн');
+
+// const firstName = 'Igor';
+// const lastname = 'Brovchuk';
+// const fullName = firstName + ' ' + lastname;
+// console.log(fullName);
+
+// const radius = 5;
+// const pi = 3.14;
+// const area = pi * radius ** 2;
+// console.log(area);
+
+// const someString = 'lorem-ipsum';
+// console.log(someString.length);
+
+// const someString2 = 'Hello world';
+// const firstLetter = someString2[0];
+// const lastLetter = someString2[someString2.length - 1]
+// console.log(firstLetter);
+// console.log(lastLetter);
+
+// const str1 = 'hello';
+// const str2 = 'world';
+// const lastLetter1 = str1[str1.length - 1];
+// const lastLetter2 = str2[str2.length - 1];
+// console.log(lastLetter1 + lastLetter2);
+
+// const userValue = 512;
+// const result = userValue * 5 ** String(userValue).length;
+// console.log(result);
+
+// const companyName = "Cyberdyne Systems";
+// const repairBots = 150;
+// const defenceBots = 50;
+// const message = `${companyName} has ${repairBots + defenceBots} bots in stock`;
+// console.log(message); // "Cyberdyne Systems has 200 bots in stock"
+
+// function foo(str1, str2) {
+//     if (str1[0] === str2[str2.length - 1])
+//         return true;
+//     else {
+//         return false;
+//     }
+// }
+// console.log(foo('hello', 'world'));
+
+// function foo(str1, str2) {
+//     if (str1.length === str2.length) {
+//         return true;
+//  }  else {
+//         return false;
+//     }
+// }
+// console.log(foo('hello', 'world'));
+
+// function foo(num) {
+//     if (String(num).length % 2 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(foo(88));
+
+// function foo(num) {
+//     return num % 2 === 1;
+// }
+// console.log(foo(5));
+
+// function foo(str) {
+//     return str !== str.toLowerCase();
+// }
+// console.log(foo('Hello'));
+
+// function foo(str) {
+//     return str.includes(' ');
+// }
+// console.log(foo('Cyberdyne Systems'));
+
+// function foo(str) {
+//     return str === str.split('').reverse().join('');
+// }
+// console.log(foo('okko'));
+
+function foo() {
+    
+}
+console.log(foo());
+
+// function foo() {
+    
+// }
+// console.log(foo());
+
+// function foo() {
+    
+// }
+// console.log(foo());
